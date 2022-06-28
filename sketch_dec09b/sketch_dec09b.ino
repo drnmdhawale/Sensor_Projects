258bc450462f53fe0081388fc5c28be7b8477feb
Nandkishor Dhawale <107688392+drnmdhawale@users.noreply.github.com>
2022-06-28 19:02:30 +0530

    Added the DAQ application code for CO2 sensor
    Arduino Program to read CO2 sensor data

---
+++ sketch_dec09b/sketch_dec09b.ino
@@ -0,0 +1,163 @@
+/*
+  LiquidCrystal Library - Hello World
+ 
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the 
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+ 
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+ 
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ 
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+
+// http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+const int numReadings2 = 3;
+int index2 = 0;
+int total2 = 0;
+int readings2[numReadings2];
+double sdev = 0;
+double average2 = 0;
+
+const int numReadings = 10;
+int readings[numReadings];      // the readings from the analog input
+unsigned long readings1[numReadings];      // the readings from the timer input
+int index = 0;                  // the index of the current reading
+int total = 0;                  // the running total
+unsigned long total1 = 0;                  // the running total
+double average = 0;                // the average
+unsigned long initial_time = millis();       //initial time
+unsigned long time =0 ;                     // average time
+
+
+int inputPin = A0;    // select the input pin for the potentiometer
+
+void setup() {
+  
+  // initialize serial communication with computer:
+  Serial.begin(9600);                   
+  // initialize all the readings to 0: 
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+    readings[thisReading] = 0;  
+  
+   for (int thisReading1 = 0; thisReading1 < numReadings; thisReading1++)
+   readings1[thisReading1] = 0;  
+ 
+ for (int thisReading2 = 0; thisReading2 < numReadings; thisReading2++)
+   readings2[thisReading2] = 0;    
+    
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("TIME V STABILITY");
+  
+}
+
+void loop() {
+  
+  delay(95);        // delay in between reads for stability
+  // subtract the last reading:
+  total= total - readings[index];   
+  total1= total1 - readings1[index];   
+  // read from the sensor:  
+  readings[index] = analogRead(inputPin); 
+  readings1[index] = millis(); 
+  // add the reading to the total:
+  total= total + readings[index];
+  total1= total1 + readings1[index];   
+  // advance to the next position in the array:  
+  index = index + 1;                    
+
+  // if we're at the end of the array...
+  if (index >= numReadings)    {          
+    // ...wrap around to the beginning: 
+    index = 0;                           
+
+  // calculate the average:
+  average = (total / numReadings);
+  time = ((total1 / numReadings) /1000) - initial_time; 
+  
+  total2 = total2 - readings2[index2];  
+  readings2[index2] = average; 
+  total2 = total2 + readings2[index2];
+  index2 = index2 + 1;
+  // if we're at the end of the array...
+  if (index2 >= numReadings2)    {          
+    // ...wrap around to the beginning: 
+  average2 = (total2 / numReadings2);
+  sdev= sqrt(( square(readings2[0] - average2) + square(readings2[1] - average2) + square(readings2[2] - average2))/3);
+  //Serial.println(sdev*0.0049);
+  //delay(1); 
+  index2 = 0;  
+  }
+  
+  // send it to the computer as ASCII digits
+  Serial.print(time);  Serial.print(',');  Serial.println(average*0.0049); 
+  delay(1);        // delay in between reads for stability     
+  
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+    // print the Sensor output in V:
+  lcd.print(time);
+  delay(1);        // delay in between reads for stability   
+  
+  lcd.setCursor(5, 1);
+    // print the Sensor output in V:
+  lcd.print(average*0.0049);
+  delay(1);        // delay in between reads for stability  
+  
+ /* lcd.setCursor(10, 1);
+    // print the Sensor output in V:
+  lcd.print(sdev*0.0049);
+  delay(1);        // delay in between reads for stability  */
+  
+  if ((sdev*0.0049) > 0.01){
+  lcd.setCursor(12, 1);
+  // print the Sensor output in V:
+  lcd.print("R");
+  delay(1);        // delay in between reads for stability  
+    
+  } else {
+  lcd.setCursor(12, 1);
+  // print the Sensor output in V:
+  lcd.print("S");
+  delay(1);        // delay in between reads for stability  
+    
+  }
+  
+  }
+    
+}
+
---
+++ sketch_mar17b/sketch_mar17b.ino
@@ -0,0 +1,136 @@
+/* Blink without Delay
+ 
+ Turns on and off a light emitting diode(LED) connected to a digital  
+ pin, without using the delay() function.  This means that other code
+ can run at the same time without being interrupted by the LED code.
+ 
+ The circuit:
+ * LED attached from pin 13 to ground.
+ * Note: on most Arduinos, there is already an LED on the board
+ that's attached to pin 13, so no hardware is needed for this example.
+ 
+ 
+ created 2005
+ by David A. Mellis
+ modified 8 Feb 2010
+ by Paul Stoffregen
+ 
+ This example code is in the public domain.
+
+ 
+ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+ */
+
+//
+String inputString = "";         // a string to hold incoming data
+//String stringOne, stringTwo, stringThree;
+boolean stringComplete = false;  // whether the string is complete
+
+  
+
+
+
+// constants won't change. Used here to 
+// set pin numbers:
+
+const int motCmdPin =  3;      // the number of the Motor pin
+int motPosPin = A0;    // select the input pin for the potentiometer
+
+
+// Variables will change:
+int motCmdState = LOW;             // ledState used to set the LED
+unsigned long initial_time = millis();       //initial time
+unsigned long time =0 ;                     //  time
+int PulseCmd=1500;                              // hh
+
+// filtering Position data
+const int numReadings = 50;
+double readings[numReadings];
+double PosValue = 0.00;  // variable to store the value coming from the sensor
+int index = 0;
+double total = 0.00;
+
+
+void setup() {
+  // set the digital pin as output:
+   pinMode(motCmdPin, OUTPUT); 
+   Serial.begin(9600);   
+   // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+   for (int thisReading = 0; thisReading < numReadings; thisReading++)
+   readings[thisReading] = 0;
+ 
+   
+}
+
+void loop()
+{
+  
+   // print the string when a newline arrives:
+  if (stringComplete) {
+             
+     if (inputString.toInt() == 1) { 
+     PulseCmd= 1500; 
+   Serial.println(inputString);}
+     
+     if (inputString.toInt() == 2) { 
+     PulseCmd= 1650;
+   Serial.println(inputString);}
+     
+     if (inputString.toInt() == 3){ 
+     PulseCmd= 1350; 
+   Serial.println(inputString);}
+     
+    // Serial.println(inputString);
+    
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+      // Motor Command
+      motCmdState = HIGH;
+      digitalWrite(motCmdPin, motCmdState);   
+      delayMicroseconds(PulseCmd);
+      motCmdState = LOW;
+      digitalWrite(motCmdPin, motCmdState);
+      delay(1);
+      
+      
+  total= total - readings[index];   
+    // read from the sensor:  
+  readings[index] = ((analogRead(motPosPin)/10)*0.3390)-1.0159; 
+    // add the reading to the total:
+  total= total + readings[index];
+  // advance to the next position in the array:  
+  index = index + 1;    
+  
+  if (index >= numReadings)    {          
+    // ...wrap around to the beginning: 
+    index = 0; 
+
+   // calculate the average:
+   PosValue = (total / numReadings);
+   time = millis() - initial_time; 
+   
+     // send it to the computer as ASCII digits
+ Serial.print(time);  Serial.print(',');  Serial.print(PulseCmd); Serial.print(','); Serial.println(PosValue); 
+  delay(1);        // delay in between reads for stability  
+  }   
+    
+  }
+    
+  void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read(); 
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    } 
+  }
+}
+
+
