8f0b8d8d6f8686b42176185fd5208bee1ba4c000
Nandkishor Dhawale <107688392+drnmdhawale@users.noreply.github.com>
2022-07-03 22:17:41 +0530

    Added Matlab Files related to crop circle
    -u

---
+++ Crop Circle/box_car_plot.m
@@ -0,0 +1,20 @@
+%% AAFC data files
+clc
+clear all
+%%
+for i = 1:17
+    j = num2str(i);
+    file = strcat('M_Data_', j, '.txt');
+    
+a = load (file);       
+R1_Mean(i,:) = mean(a);
+R1_SD(i,:) = std(a);
+
+end
+
+subplot(3,1,1), plot(R1_Mean(:,1))
+grid on
+subplot(3,1,2), plot(R1_Mean(:,2))
+grid on
+subplot(3,1,3), plot(R1_Mean(:,3))
+grid on
